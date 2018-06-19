234b48831a18af468cd68faa130a7c2660802f86
iKenshu <undefined.hlo.o@gmail.com>
2018-06-19 11:46:09 -0400

    course of React on Platzi

---
+++ index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import Media from './src/playlist/components/media'
+
+const app = document.getElementById('app')
+
+render(<Media />, app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './media.css'
+
+class Media extends Component {
+    render() {
+        const styles = {
+            container: {
+                fontSize: 14,
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid red',
+            }
+        }
+        return (
+            <div className='Media'>
+                <div className='Media-cover'>
+                    <img className='Media-image'
+                        src="./images/covers/bitcoin.jpg" 
+                        width={260}
+                        height={160}
+                        alt=""
+                    />
+                    <h3 className='Media-title'>¿Por qué aprender React?</h3>
+                    <p className='Media-author'>Leonidas Esteban</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Media
